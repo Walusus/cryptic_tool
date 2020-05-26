f4d677426ce8a782bfb4a72168fe43919adb08ef
knowosadko <kmnowosadko@gmail.com>
2020-05-26 15:58:28 +0200

    Added code comments in generator file.

--- generator.py
+++ generator.py
@@ -3,7 +3,18 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
+"""
+    Schemat wywołania:
+    generatot [<opcje>] <nazwa pliku do od>
 
+Opcje:
+    -p, --private-key <nazwa pliku> - Ścieżka do klucza prywatnego, na podstawie którego ma zostać wygenerowany publiczny.
+    -s, --size <rozmiar> - Rozmiar klucza do wygenerowania w bajtach.
+    -h, --help - Wyświetlenie tej notki mw.
+
+Info:
+    Generuje klucz publiczny lub parę kluczy (prywatny i publiczny). Domyślnym wyjściem są pliki public.pem i private.pem.
+"""
 def main(**args):
     if args['both']:
         # generates two keys
@@ -14,16 +25,18 @@ def main(**args):
             format=serialization.PrivateFormat.TraditionalOpenSSL,
             encryption_algorithm=serialization.NoEncryption()
         )
+        # saving private key
         with open(args['private-key'], 'wb') as pem_out:
             pem_out.write(pem)
         pub_pem = public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
+        # saving public keys
         with open(args['private-key']+".pub", 'wb') as pem_out:
             pem_out.write(pub_pem)
     else:
-        print("Only public")
+        # generates one public key suing existing private key
         with open(args['private-key'], "rb") as key_file:
             private_key = serialization.load_pem_private_key(key_file.read(), password=None, backend=default_backend())
             public_key = private_key.public_key()
@@ -31,16 +44,16 @@ def main(**args):
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo
             )
+            # saving public key
             with open(args['private_key'] + ".pub", 'wb') as pem_out:
                 pem_out.write(pub_pem)
 
 
 if __name__ == '__main__':
-    # Terminal arguments
-    parser = argparse.ArgumentParser(prog='generator', description='Info: \n Generuje klucz prywatny lub parę kluczy (prywatny i publiczny). Domyślnym wyjściem są pliki public.pem i private.pem.')
-    parser.add_argument('-p', '--private-key', dest="private-key",type=str, required=True, help='Name of private key to use to genearte public key.')
-    parser.add_argument('-s', '--size', type=int, dest="size",default=2048, required=False , help="Size of key to generate in bytes.")
-    parser.add_argument('-b', '--both', type=bool, dest="both",default=True, required=False, help="Size of key to generate in bytes.")
+    parser = argparse.ArgumentParser(prog='generator', description='Info: \n Generates public key or pair of keys (public and private) default output is filename.pub and filename.')
+    parser.add_argument('-p', '--private-key', dest="private-key", type=str, required=True, help='Name of private key to use to genearte public key.')
+    parser.add_argument('-s', '--size', type=int, dest="size", default=2048, required=False , help="Size of key to generate in bytes.")
+    parser.add_argument('-b', '--both', type=bool, dest="both", default=True, required=False, help="Size of key to generate in bytes.")
     parser.add_argument('--password', type=str, required=False, help="Password used to acces private key.")
     parser.set_defaults()
     args = parser.parse_args()
